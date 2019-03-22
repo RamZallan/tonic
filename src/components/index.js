5b5bd2f7646f5842f7b26da6f10a3a4d09215ed9
Ram Zallan <ramzallan@gmail.com>
2019-03-22 18:46:20 -0400

    Remove old import

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Create } from './Create';
 export { default as Home } from './Home';
 export { default as NavBar } from './NavBar';
 export { default as ItemList } from './ItemList';
