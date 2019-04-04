fef1bdefac1aa4ab085334be87e82c905f294109
Ram Zallan <ramzallan@gmail.com>
2019-04-03 22:49:35 -0400

    Fix Navbar components, implement React Router routing

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import {Collapse, Container, Nav, Navbar, NavbarBrand, NavbarToggler, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import { NavLink as RouterNavLink } from "react-router-dom";
 import UserProfile from '../../containers/UserProfile';
 
 class NavBar extends React.Component {
@@ -24,38 +25,55 @@ class NavBar extends React.Component {
             <div>
                 <Navbar color="primary" dark expand="md" fixed="top">
                     <Container>
-                        <NavbarBrand href="/">WebDrink</NavbarBrand>
+                        <NavbarBrand
+                            activeClassName="active"
+                            exact={true}
+                            tag={RouterNavLink}
+                            to={'/'}>
+                                WebDrink
+                        </NavbarBrand>
                         <NavbarToggler onClick={this.toggle}/>
                         <Collapse isOpen={this.state.isOpen} navbar>
                             <Nav navbar>
-                                <div>
-                                    {
-                                    this.props.isDrinkAdmin && 
-                                    <NavItem>
-                                        {/* Todo make more atomic, AdminDropdown */}
-                                        <UncontrolledDropdown nav inNavbar>
-                                            <DropdownToggle nav caret className="navbar-user">
-                                                Admin
-                                                <span className="caret"/>
-                                            </DropdownToggle>
-                                            <DropdownMenu>
-                                                <DropdownItem href={"/users"}>
-                                                    Users
-                                                </DropdownItem>
-                                                <DropdownItem href={"/items"}>
-                                                    Items
-                                                </DropdownItem>
-                                                <DropdownItem href={"/temps"}>
-                                                    Temps
-                                                </DropdownItem>
-                                                <DropdownItem href={"/logs"}>
-                                                    Logs
-                                                </DropdownItem>
-                                            </DropdownMenu>
-                                        </UncontrolledDropdown>
-                                    </NavItem>
-                                    }
-                                </div>
+                                {
+                                    this.props.isDrinkAdmin &&
+                                    <UncontrolledDropdown nav inNavbar>
+                                        <DropdownToggle nav caret className="navbar-user">
+                                            Admin
+                                            <span className="caret"/>
+                                        </DropdownToggle>
+                                        <DropdownMenu>
+                                            <DropdownItem
+                                              activeClassName="active"
+                                              exact={true}
+                                              tag={RouterNavLink}
+                                              to={'/users'}>
+                                                Users
+                                            </DropdownItem>
+                                            <DropdownItem
+                                              activeClassName="active"
+                                              exact={true}
+                                              tag={RouterNavLink}
+                                              to={'/items'}>
+                                                Items
+                                            </DropdownItem>
+                                            <DropdownItem
+                                              activeClassName="active"
+                                              exact={true}
+                                              tag={RouterNavLink}
+                                              to={'/temps'}>
+                                                Temps
+                                            </DropdownItem>
+                                            <DropdownItem
+                                              activeClassName="active"
+                                              exact={true}
+                                              tag={RouterNavLink}
+                                              to={'/logs'}>
+                                                Logs
+                                            </DropdownItem>
+                                        </DropdownMenu>
+                                    </UncontrolledDropdown>
+                                }
                             </Nav>
                             <Nav navbar className="ml-auto">
                                 <UserProfile/>
